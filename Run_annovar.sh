aa385ab925daff1bbbfca1024f3975d49852fc54
raulmejia <raulmejia@users.noreply.github.com>
2021-04-09 14:21:48 +0200

    Update Run_annovar.sh

--- Run_annovar.sh
+++ Run_annovar.sh
@@ -6,12 +6,28 @@
 
 
 
-# Download the databases to the folder humandb
+# Download the databases in some place in your computer under the folder humandb
+```
 perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar refGene humandb/
 perl annotate_variation.pl -buildver hg19 -downdb cytoBand humandb/
 perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar exac03 humandb/
 perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar avsnp147 humandb/
-perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar dbnsfp30a humandb/
+perl annotate_variation.pl -buildver hg19 -downdb -webfrom annovar dbnsfp30a humandb/  
+```
+
+If you add to the path annotate_variation.pl you can run it only with "annotate_variation.pl arg1 arg2"  
+
+You can download the following databases
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar gnomad_genome humandb/  
+gnomad_genome has a more comprehensive AF than exac03, the later is quite sparse
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar 1000g2015aug humandb/
+It threw me an error whan I ran it because it could not locate 1000g2015aug.txt
+annotate_variation.pl -buildver hg19 -downdb -webfrom annovar clinvar_20160302 humandb/  
+clinical anotations
+
+
+In the user guide (filter, gene or region) you can find the explanation of each anotation https://annovar.openbioinformatics.org/en/latest/user-guide/filter/
+
 
 $vcfsource_folder/your.vcf
 humandb4annovar=/path/to/humandb/
